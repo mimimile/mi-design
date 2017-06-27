12ff4bf4cbb369f1042380150f822a62f8fed551
xiaomi <391147815@qq.com>
2017-06-27 17:20:12 +0800

    增加了例子界面

---
+++ .postcssrc.js
@@ -0,0 +1,17 @@
+/*
+* @Author: xiaomi
+* @Date:   2017-06-27 13:44:16
+* @Last Modified by:   xiaomi
+* @Last Modified time: 2017-06-27 13:44:34
+*/
+
+'use strict';
+
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    // to edit target browsers: use "browserlist" field in package.json
+    "autoprefixer": {}
+  }
+}
